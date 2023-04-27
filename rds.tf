2930a9ef11dbb6bb2332085786f5b9199c0a9d68
CodingManoj <mvp2612@hotmail.com>
2023-04-26 23:06:40 -0400

    Git Auto Push

--- rds.tf
+++ rds.tf
@@ -1,11 +1,11 @@
 # Creates RDS Instance 
 resource "aws_db_instance" "mysql" {
   # identifier              = "robot-${var.ENV}"
-  allocated_storage       = 10
+  allocated_storage       = var.MYSQL_RDS_STORAGE
   db_name                 = "mydb"
   engine                  = "mysql"
-  engine_version          = "5.7"
-  instance_class          = "db.t3.micro"
+  engine_version          = var.MYSQL_RDS_ENGINE_VERSION
+  instance_class          = var.MYSQL_RDS_INSTANCE_TYPE
   username                = "admin1"
   password                = "RoboShop1"
   parameter_group_name    = aws_db_parameter_group.mysql_pg.name
@@ -17,7 +17,7 @@ resource "aws_db_instance" "mysql" {
 
 resource "aws_db_parameter_group" "mysql_pg" {
   name   = "robot-${var.ENV}-mysql-pg"
-  family = "mysql5.7"
+  family = "mysql${var.MYSQL_RDS_ENGINE_VERSION}"
 }
 
 resource "aws_db_subnet_group" "mysql_subnet_group" {
