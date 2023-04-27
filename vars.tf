9bd89cd19068d933da317d04385cd2d5aa8b04e1
CodingManoj <mvp2612@hotmail.com>
2023-04-26 23:07:40 -0400

    Git Auto Push

--- vars.tf
+++ vars.tf
@@ -2,4 +2,4 @@ variable "ENV" {}
 variable "MYSQL_RDS_PORT" {}
 variable "MYSQL_RDS_STORAGE" {}
 variable "MYSQL_RDS_ENGINE_VERSION" {}
-variable "MYSQL_RDS_INSTANCE_TYPE {}
+variable "MYSQL_RDS_INSTANCE_TYPE" {}
